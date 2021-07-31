2a438238a838cdebb92507618cd28def581e86c0
Jacob T. Armentrout <jacoba973@gmail.com>
2021-07-31 12:54:21 -0400

    write to file functionality

--- problem.py
+++ problem.py
@@ -1,18 +1,19 @@
 print("Collatz Conjecture")
 f = open('collatz-output.txt', 'a')
 amount = float(input('Principal Amount? '))
+f.write('\n' + str(amount) + ' ')
 
 while amount > 1:
 
     if (amount % 2) != 0:
         amount = ((3 * amount) + 1)
         # print(amount)
-        f.write(amount)
+        f.write(str(amount) + ' ')
 
     elif (amount % 2) == 0:
         amount = (amount / 2)
         # print(amount)
-        f.write(amount)
+        f.write(str(amount) + ' ')
 
     else:
         print('Error')
